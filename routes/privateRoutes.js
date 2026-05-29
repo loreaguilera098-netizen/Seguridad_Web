477a80763f9c00ac373558985ab0cadf3895bccc
loreaguilera098-netizen <loreaguilera098@gmail.com>
2026-05-28 20:26:57 -0400

    Update privateRoutes.js

--- routes/privateRoutes.js
+++ routes/privateRoutes.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { requireAuth } = require('../middleware/authMiddleware');
+const requireAuth = require('../middleware/authMiddleware');
 
-// Ruta protegida
 router.get('/dashboard', requireAuth, (req, res) => {
-    res.send(`
-        <h1>Área Privada</h1>
-        <p>Bienvenido <strong>${req.session.user.username}</strong></p>
-        <p>Rol: ${req.session.user.role}</p>
-        <a href="/auth/logout">Cerrar sesión</a>
-        <br>
-        <a href="/login.html">Volver al login</a>
-    `);
+  res.sendFile('dashboard.html', { root: './views' });
 });
 
 module.exports = router;
